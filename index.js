bd504c43b1dfd06f995b483b6945c3fd4010c106
Eduardo Hernandez <egh1795@gmail.com>
2018-05-19 07:53:50 -0500

    Change in mongo url, because i was using the mlab from another app deployed in heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var mongoose =  require ('mongoose');
 var app = require('./app');
-var uriString= 'mongodb://guanahack:guanahack@ds151117.mlab.com:51117/heroku_j3swpk1x';
+var uriString= 'mongodb://guanahack:guanahack@ds229450.mlab.com:29450/heroku_7qm53lkv';
 //var uriString = 'mongodb://heroku_h3hq9mht:Morita1234@ds159129.mlab.com:59129/heroku_h3hq9mht';
 var port = process.env.PORT || 5000;
 
