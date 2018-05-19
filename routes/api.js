d2bcf4204fd6417aad6d6bfe5a785067f1e6bc13
Erick Martinez <erick.e.mtz.g@gmail.com>
2018-05-19 01:34:30 -0500

    Add google speech

--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,7 @@ var md_auth = require('../middlewares/authenticated');
 var multiPart = require('connect-multiparty');
 var md_upload =  multiPart({uploadDir:'./uploads/users'});
 
-api.get('/dangerous',apiController.home)
+api.get('/dangerous',apiController.home);
+api.post('/dangerous', apiController.saveInBlackList);
 
 module.exports = api;
